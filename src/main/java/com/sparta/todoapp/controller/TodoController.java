f05647d630072eb2292fc48f52ceab47a68f0070
Sehee Hong <seaheehong@gmail.com>
2024-02-07 14:33:15 +0900

    feat(todo) : 일정 목록 조회 기능 추가

--- src/main/java/com/sparta/todoapp/controller/TodoController.java
+++ src/main/java/com/sparta/todoapp/controller/TodoController.java
@@ -6,23 +6,36 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequestMapping("/v1.0/todo")
 @RestController
 @AllArgsConstructor
 public class TodoController {
 
     public final TodoService todoService;
 
-    @PostMapping("/v1.0/todo")
+    @PostMapping
     public ResponseEntity<TodoResponseDto> postTodo(@RequestBody TodoRequestDto dto) {
         Todo todo = todoService.createTodo(dto);
         TodoResponseDto response = new TodoResponseDto(todo);
         return ResponseEntity.ok().body(response);
     }
 
-    @GetMapping("/v1.0/todo/{todoId}")
+    @GetMapping("/{todoId}")
     public ResponseEntity<TodoResponseDto> getTodo(@PathVariable Long todoId) {
         Todo todo = todoService.getTodo(todoId);
         TodoResponseDto response = new TodoResponseDto(todo);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TodoResponseDto>> getTodos() {
+        List<Todo> todos = todoService.getTodos();
+        List<TodoResponseDto> response = todos.stream()
+                .map(TodoResponseDto::new)
+                .collect(Collectors.toList());
         return ResponseEntity.ok().body(response);
     }
 }
--- src/main/java/com/sparta/todoapp/service/TodoService.java
+++ src/main/java/com/sparta/todoapp/service/TodoService.java
@@ -4,7 +4,10 @@ import com.sparta.todoapp.controller.TodoRequestDto;
 import com.sparta.todoapp.repository.Todo;
 import com.sparta.todoapp.repository.TodoRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -22,5 +25,10 @@ public class TodoService {
     public Todo getTodo(Long todoId) {
         return todoRepository.findById(todoId)
                 .orElseThrow(IllegalArgumentException::new);
+    }
+
+    // 할일 전체 조회
+    public List<Todo> getTodos() {
+        return todoRepository.findAll(Sort.by("createdAt").descending());
     }
 }
